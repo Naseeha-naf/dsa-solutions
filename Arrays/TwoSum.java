df1938f02f95b97fb7ccbd8f7c956f6cee67996c
Naseeha 001 <naseehanafrin.24it@kct.ac.in>
2026-06-02 23:59:45 +0530

    Update TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,1 +1,15 @@
+class Solution{
+    public int[] twoSum(int []nums,int target){
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){ //a+b=sum
+                    return new int[]{i,j}; //returning its index
+                }
+             
+            }
+        }
+           return new int[]{-1,-1}; ///return if not found
+    }
+    
+}
 
