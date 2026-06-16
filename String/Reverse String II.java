fa42c033a16446dd255311e4a7dad696c6bd4005
Naseeha 001 <naseehanafrin.24it@kct.ac.in>
2026-06-17 00:37:16 +0530

    Create Reverse String II.java

---
+++ String/Reverse String II.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        for (int i = 0; i < n; i += 2 * k) {
+            int start = i;
+            int end = Math.min(i + k - 1, n - 1);
+
+            reverse(arr, start, end);
+        }
+
+        return new String(arr);
+    }
+
+    private void reverse(char[] arr, int l, int r) {
+        while (l < r) {
+            char temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
